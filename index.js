4b430980b7913a8006bcc8135b8b6026f51b9078
Oliver Wagner <oliver.wagner@student.reutlingen-university.de>
2018-10-08 17:02:47 +0200

    timestamp

--- index.js
+++ index.js
@@ -2,25 +2,25 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
-    socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
-  });
-    socket.on('chat message', function(msg){
-    socket.broadcast.emit('chat message', msg);
-  });
+io.on('connection', function (socket) {
+    alert();
+    socket.on('disconnect', function () {
+        console.log('user disconnected');
+        socket.emit('Willkommen im Chat');
+    });
+    socket.on('chat message', function (msg) {
+        console.log('message: ' + msg);
+    });
+    socket.on('chat message', function (msg) {
+        socket.broadcast.emit('chat message', msg);
+    });
 });
 
 
-
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(3000, function () {
+    console.log('listening on *:3000');
 });
