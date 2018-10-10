11124f3bd2e62881aeb6d18f4a13f86e0c1da9a8
Oliver Wagner <oliver.wagner@student.reutlingen-university.de>
2018-10-10 11:08:02 +0200

    pub

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var date = require('dateformat');
+var time = date(new Date(), "HH:MM");
+var users = [];
+
+app.use(express.static('pub'));
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function (socket) {
-    alert();
+    socket.emit('chat message', time+' Willommen im Tschätt');
+    console.log('a user connected');
     socket.on('disconnect', function () {
         console.log('user disconnected');
-        socket.emit('Willkommen im Chat');
     });
     socket.on('chat message', function (msg) {
-        console.log('message: ' + msg);
+        console.log('message: ' + time+' '+msg);
     });
     socket.on('chat message', function (msg) {
-        socket.broadcast.emit('chat message', msg);
+        socket.broadcast.emit('chat message', time+" "+msg);
     });
 });
 
---
+++ main.js
@@ -0,0 +1,17 @@
+$(function () {
+    console.log(1);
+    var socket = io();
+    $("#send").submit(function () {
+        socket.emit('chat message', $('#m').val());
+        $('#m').val('');
+        return false;
+    });
+    socket.on('chat message', function (msg) {
+        $('#messages').append($('<li>').text(msg));
+    });
+
+    $("#upload").submit(function (req, res) {
+        console.log(req);
+        return false;
+    });
+});
