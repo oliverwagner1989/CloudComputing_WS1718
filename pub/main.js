25842419f0f3fa83b0c744d949db69de668afc40
Oliver Wagner <oliver.wagner@student.reutlingen-university.de>
2018-10-11 23:30:16 +0200

    upload: little bugfix

--- pub/main.js
+++ pub/main.js
@@ -1,36 +1,29 @@
 $(function () {
-    console.log(1);
     var socket = io();
-    console.log(2);
     var username = prompt("What's your Username?");
     console.log(username);
     socket.emit('add user', username);
-    console.log(3);
     socket.on('user joined', (data) => {
         console.log(data.username + ' joined');
         $('#messages').append($('<li>').text(data.username + ' joined'));
     });
 
-    console.log(4);
     $("#send").submit(function () {
         socket.emit('chat message', $('#m').val());
         $('#m').val('');
         return false;
     });
-    console.log(5);
+
     socket.on('chat message', function (msg) {
         $('#messages').append($('<li>').text(' ' + msg));
     });
-    console.log(6);
-    $("#upload_btn").click(function () {
-        upload(socket);
-        console.log(7);
-    });
+
+    upload(socket);
+
 
 });
 
 function upload(socketio) {
-    console.log(8);
     // Initialize instances:
     var socket = socketio.connect();
     var siofu = new SocketIOFileUpload(socket);
@@ -52,5 +45,4 @@ function upload(socketio) {
         console.log(event.success);
         console.log(event.file);
     });
-    console.log(9);
 }
