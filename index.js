56078624425120860fc4695fcf25b22b9d1dc37c
Oliver Wagner <oliver.wagner@student.reutlingen-university.de>
2018-11-19 22:58:10 +0100

    removed tone analzyer

--- index.js
+++ index.js
@@ -59,15 +59,6 @@ var userlist = [];
 var usermap = {}; //Hashmap safes socket.id for whisper mode
 var fileWhispername;
 var fileWhisperID;
-var ToneAnalyzerV3 = require('watson-developer-cloud/tone-analyzer/v3');
-var bodyParser = require('body-parser');
-
-require('dotenv').config({silent: true});
-
-// Create the service wrapper
-var toneAnalyzer = new ToneAnalyzerV3({
-  version_date: '2017-09-21',
-});
 
 app.use(bodyParser.json());
 
@@ -75,65 +66,6 @@ app.use(express.static('pub'));
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
-});
-
-//Mood Analyzer
-function createToneRequest (request) {
-  let toneChatRequest;
-
-  if (request.texts) {
-    toneChatRequest = {utterances: []};
-
-    for (let i in request.texts) {
-      let utterance = {text: request.texts[i]};
-      toneChatRequest.utterances.push(utterance);
-    }
-  }
-
-  return toneChatRequest;
-}
-
-function happyOrUnhappy (response) {
-  const happyTones = ['satisfied', 'excited', 'polite', 'sympathetic'];
-  const unhappyTones = ['sad', 'frustrated', 'impolite'];
-
-  let happyValue = 0;
-  let unhappyValue = 0;
-
-  for (let i in response.utterances_tone) {
-    let utteranceTones = response.utterances_tone[i].tones;
-    for (let j in utteranceTones) {
-      if (happyTones.includes(utteranceTones[j].tone_id)) {
-        happyValue = happyValue + utteranceTones[j].score;
-      }
-      if (unhappyTones.includes(utteranceTones[j].tone_id)) {
-        unhappyValue = unhappyValue + utteranceTones[j].score;
-      }
-    }
-  }
-  if (happyValue >= unhappyValue) {
-    return 'happy';
-  }
-  else {
-    return 'unhappy';
-  }
-}
-
-app.post('/tone', (req, res, next) => {
-  let toneRequest = createToneRequest(req.body);
-
-  if (toneRequest) {
-    toneAnalyzer.toneChat(toneRequest, (err, response) => {
-      if (err) {
-        return next(err);
-      }
-      let answer = {mood: happyOrUnhappy(response)};
-      return res.json(answer);
-    });
-  }
-  else {
-    return res.status(400).send({error: 'Invalid Input'});
-  }
 });
 
 io.on('connection', function (socket) {
--- pub/main.js
+++ pub/main.js
@@ -87,7 +87,6 @@ $(function () {
          //empty messages cannot be send
         } else if ($('#m').val().trim()!='') {
             socket.emit('chat message', $('#m').val());
-			getTone();
 			
         } else {
             alert("input error");
@@ -146,30 +145,3 @@ $(function () {
         });
     }
 });
-
-    function getTone() {
-        fetch("/tone", {
-            method: "POST",
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-                'mode': 'cors'
-            },
-            body: JSON.stringify({
-               texts: [document.getElementById("m").value]
-            })
-        })
-        .then((response) => {
-            var contentType = response.headers.get("content-type");
-            if(contentType && contentType.includes("application/json")) {
-               return response.json();
-            }
-            throw new TypeError("Oops, we haven't got JSON!");
-        })
-        .then((response) => { 
-            console.log("response:" +  JSON.stringify(response));
-            if (response.mood) {
-              socket.emit('I am '+response.mood);
-            }
-        })
-    }
